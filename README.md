7d9cffe9c85ecb66fd68ec175b3b4c1e528e749f
Dave Baird <dave@zerofive.co.uk>
2021-11-04 00:24:29 +0100

    No log message supplied

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
     my $tries = 5 ;     # default 0 - keep trying forever
     my $delay = 2 ;     # default 1 second
 
-    my $file = repeat_until { get_locked_file() }, $tries, $delay ;
+    my $file = repeat_until { get_locked_file() } $tries, $delay ;
 
     write_stuff_to( $file ) if $file ;
 
@@ -23,6 +23,15 @@ Context aware - the coderef is run in the same list or scalar context as `repeat
 
 In void context, the coderef is run in scalar context, but no value is returned when the coderef returns true.
 
+# GOTCHA!
+
+Note there is no comma after the coderef in the argument list. This will cause you pain if you forget it.
+The error generated is:
+
+    Useless use of private variable in void context
+
+BUT Perl reports the wrong line.
+
 ## Exports
 
 The module exports a single subroutine.
@@ -33,4 +42,4 @@ The module exports a single subroutine.
 
     `$repeats` defaults to `0`, meaning repeat indefinitely until coderef returns true.
 
-    `$repeats` defaults to `1` second.
+    `$delay` defaults to `1` second.
--- lib/Repeat/Until.pm
+++ lib/Repeat/Until.pm
@@ -1,4 +1,5 @@
 package Repeat::Until ;
+
 use v5.20 ;
 use warnings ;
 
@@ -7,6 +8,7 @@ use Exporter qw(import) ;
 our @EXPORT = qw(repeat_until) ;
 
 use feature qw(signatures) ;
+
 no warnings qw(experimental::signatures) ;
 
 =pod
@@ -22,7 +24,7 @@ C<Repeat::Until> - run a coderef until it returns true
     my $tries = 5 ;     # default 0 - keep trying forever
     my $delay = 2 ;     # default 1 second
 
-    my $file = repeat_until { get_locked_file() }, $tries, $delay ;
+    my $file = repeat_until { get_locked_file() } $tries, $delay ;
 
     write_stuff_to( $file ) if $file ;
 
@@ -35,6 +37,15 @@ The return value is the return value of the coderef.
 Context aware - the coderef is run in the same list or scalar context as C<repeat_until> is called.
 
 In void context, the coderef is run in scalar context, but no value is returned when the coderef returns true.
+
+=head1 GOTCHA!
+
+Note there is no comma after the coderef in the argument list. This will cause you pain if you forget it.
+The error generated is:
+
+    Useless use of private variable in void context
+
+BUT Perl reports the wrong line.
 
 
 =head2 Exports
@@ -50,11 +61,12 @@ C<$repeats> and C<$delay> are optional.
 
 C<$repeats> defaults to C<0>, meaning repeat indefinitely until coderef returns true.
 
-C<$repeats> defaults to C<1> second.
+C<$delay> defaults to C<1> second.
 
 =back
 
 =cut
+
 
 sub repeat_until : prototype(&;$$) ( $cr, $reps = 0, $interval = 1 ) {
 
